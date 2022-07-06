7a50bf8e593c3c085620db8c35b8388d9f3ba3a8
Melanny <melanny.lima@outlook.com>
2022-07-06 18:45:12 -0400

    created Game class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log("Tamagotchi");
+
+class Game {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+
+  }
+
+  onFeed() {
+    console.log("num num");
+  }
+  onSleep() {
+    console.log("Zzz...");
+  }
+  onPlay() {
+    console.log("hehe");
+  }
+}
