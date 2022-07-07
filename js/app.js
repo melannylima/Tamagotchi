245004050e96dbbc455f90d9aed096491c7fc662
Melanny <melanny.lima@outlook.com>
2022-07-06 20:49:50 -0400

    eggs are buttons

--- js/app.js
+++ js/app.js
@@ -18,4 +18,32 @@ class Game {
   }
 }
 
-const tamagotchi = new Game(nick, type)
+// const tamagotchi = new Game(nick, type)
+
+function selector() {
+  const bod = document.getElementById("interactive")
+  const eggs = document.createElement("DIV")
+  eggs.setAttribute("id", "egg-buttons")
+  const egg1 = document.createElement("BUTTON")
+  const egg2 = document.createElement("BUTTON")
+  const egg3 = document.createElement("BUTTON")
+  egg1.setAttribute("id", "egg1")
+  egg2.setAttribute("id", "egg2")
+  egg3.setAttribute("id", "egg3")
+  egg1.innerHTML = '<img src="img/egg1.png" />';
+  egg2.innerHTML = '<img src="img/egg2.png" />';
+  egg3.innerHTML = '<img src="img/egg3.png" />';
+  eggs.appendChild(egg1)
+  eggs.appendChild(egg2)
+  eggs.appendChild(egg3)
+  bod.appendChild(eggs)
+}
+
+
+
+
+
+
+
+
+document.getElementById("init").addEventListener("click", selector)
